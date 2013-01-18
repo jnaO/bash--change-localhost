c3f6649c5ee86e7903bae9e280a04d9565977590
Jan Lofberg <chenebu@gmail.com>
2013-01-18 23:19:30 +0100

    added support for absolute ad soft paths

--- c-lhost.sh
+++ c-lhost.sh
@@ -1,10 +1,60 @@
 #!/bin/bash
 
-# Start processing options at index 1.
-OPTIND=1
 
-WEBROOT=`pwd`'/'$1
-# ALTROOT=${1:-$WEBROOT}
+# And here we have Bash Patterns:
+
+USERARG=$1
+BASE=`pwd`
+WEBROOT=''
+
+traverse() {
+  # Recursive function, make initial check to see if we
+  # need to substring the paths
+  if [[ "${USERARG}" == ../* ]]
+  then
+    # Split the currently working directory (or what's
+    # left of it), into an array
+    IFS='/' read -ra SOFTPATH <<< "${BASE}"
+    # For readability, store the string to be trimmed in a variable
+    REMOVE_FROM_BASE='/'${SOFTPATH[${#SOFTPATH[@]}-1]}
+
+    # Remove the first (#) '../' from the soft path passed in
+    USERARG=${USERARG#'../'}
+    # Remove the last (%) folder from the current working directory
+    BASE=${BASE%${REMOVE_FROM_BASE}}
+    # se if user want to go higher in the file tree
+    traverse $BASE $USERARG
+  else
+    WEBROOT=$BASE'/'$USERARG
+    if [ -d "${WEBROOT}" ] ; then
+      echo ""
+      echo "Changing your web root to:"
+      echo ""
+      echo " ~---=== $WEBROOT ===---~ "
+      echo ""
+      echo ""
+    else
+      echo ""
+      echo "$WEBROOT is not a folder."
+      echo "You need to pass in the path to a folder that you wan't to use as your webroot."
+      echo "-------------------------------------------------------------------------------"
+      echo ""
+    fi
+  fi
+}
+
+if [[ "$USERARG" == /* ]]
+then
+  WEBROOT=$USERARG
+  echo $WEBROOT
+elif [[ "$1" == ../* ]]
+then
+  echo 'not directly below current working directory'
+  traverse $BASE $USERARG
+else
+  WEBROOT=`pwd`'/'$USERARG
+  echo $WEBROOT
+fi
 
 rm ~/Sites/localhost
 ln -s $WEBROOT ~/Sites/localhost
--- readme.md
+++ readme.md
@@ -7,15 +7,8 @@ Create a symlink to a folder on your path (run `echo $PATH` in terminal to find 
 
 * `ln -s path/to/change_localhost/c-lhost.sh chost`
 * restart your terminal
-* run `chost` in the folder you want to use as localhost, or pass in relative path (directly below your current position only) as an argument as follows:
+* run `chost` in the folder you want to use as localhost, or pass in a path as an argument as follows:
 
     `chost client/projname/siteroot/`
 
 Make sure your webserver is pointing towards ~/Sites/localhost.
-
-
-Known issues
-============
-
-As of right now this scrip can not handle relative paths that traverse upwards, I.E. `../somefolder/desired_webroot/` will not work.
-Neither can this script handle absolute paths, I.E. `/Users/username/web_root/` or `~/web_root/`
